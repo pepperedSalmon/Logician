1eadb0004d02eec211007d2983b770897d11fa27
Ivan <ivanlg428@gmail.com>
2021-10-20 19:33:16 -0700

    setup

--- Logician/Logician.py
+++ Logician/Logician.py
@@ -1,1 +1,17 @@
+def solve(m,k1x,k2x):
+    pass
+
+
+
+
+if __main__=="__main__":
+    m=[[0,0,1,1,1],
+       [0,1,1,1,1],
+       [1,1,0,1,1],
+       [1,1,1,0,0,],
+       [1,1,1,0,0,]]
+
+    k1=2;# number of non-contradictions
+    k2=1;# number of contradications
+    solve(m,k1,k2)
 
